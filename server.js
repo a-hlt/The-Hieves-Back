adb46fd2b302383c07ab61cca7af19ca1ff2aa43
zharrow <florent.detres@protonmail.com>
2025-01-28 13:58:52 +0100

    init user

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
-import app from './app.js'
-// import prisma from './db.js'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import app from './app.js';
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT );
-})
+// Création du serveur HTTP
+const httpServer = createServer(app);
 
-// prisma.$connect().then(async () => {
-//     console.log('Connected to the database')
+// Configuration de Socket.IO
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173", // Remplace par l'URL de ton application React
+    methods: ["GET", "POST"],
+  },
+});
 
-//     app.listen(PORT, () => {
-//         console.log('Server is running on port 3000');
-//     })
+// Gestion des événements Socket.IO
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-// }).catch((error) => {
-//     console.error(error)
-// })
+  socket.on("sendMessage", (data) => {
+    // Émission du message à tous les clients
+    io.emit("message", { id: Date.now(), type: "user", text: data });
+
+    // Simuler une réponse automatique
+    setTimeout(() => {
+      io.emit("message", {
+        id: Date.now() + 1,
+        type: "bot",
+        text: "Alerte : veuillez vous éloigner des zones indiquées !",
+      });
+    }, 1000);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Lancer le serveur HTTP
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
