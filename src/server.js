61d807651e94705540530f13932fdab8bdb540b3
Madjinda Lucas <lucasmadjinda@gmail.com>
2025-01-28 09:53:49 +0100

    modification du fichier serveur pour communiquer avec le front pour socket.io

--- src/server.js
+++ src/server.js
@@ -4,26 +4,40 @@ const { Server } = require("socket.io");
 const routes = require("./routes/index");
 
 const app = express();
+
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://localhost:5173", // Remplacez par l'URL de votre application React
+		methods: ["GET", "POST"],
+	},
+});
 
 app.use(express.json());
 app.use("/api", routes);
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+	console.log("User connected:", socket.id);
 
-  socket.on("message", (data) => {
-    console.log("Message received:", data);
-    io.emit("message", data);
-  });
+	socket.on("sendMessage", (data) => {
+		console.log("Message received:", data);
+		io.emit("message", { id: Date.now(), type: "user", text: data });
+		// Simuler une réponse automatique
+		setTimeout(() => {
+			socket.emit("message", {
+				id: Date.now() + 1,
+				type: "bot",
+				text: "Alerte veuillez vous éloignez le plus possible des zones indiqués !  ",
+			});
+		}, 1000);
+	});
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+	socket.on("disconnect", () => {
+		console.log("User disconnected:", socket.id);
+	});
 });
 
 const PORT = 4000;
 httpServer.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
