818224e7ad94b92bde93f23633ea8b98036c33a0
devilsc0d3 <faureleo736@gmail.com>
2025-01-28 09:22:40 +0100

    chore/clean-code

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import cors from "cors";
 import app from "express/lib/application.js";
+import {Server} from "socket.io";
 import usersRouter from "./routes/usersRouter.js";
+import {createServer} from "http";
+import routes from "./routes";
 
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 app.use(express.json());
 
@@ -12,10 +18,27 @@ app.use(cors());
 // Add a basic route
 const version = "/v1";
 app.use(version + "/users", usersRouter);
+app.use(version + "/api", routes);
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("message", (data) => {
+        console.log("Message received:", data);
+        io.emit("message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+
 
 // start server
-app.listen(3000, () => {
-    console.log('http://localhost:3000/');
+const PORT = 4000;
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
