8706c13c38a05c53089d2d36659b2b55bca33d27
Aubin <heuillet.aubin@gmail.com>
2025-01-28 16:13:16 +0100

    add getEventByZone

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,4 +1,7 @@
 import * as eventService from "../services/eventServices.js";
+import { Prisma } from "@prisma/client";
+import { getEventsByZoneService } from '../services/eventServices.js';
+
 
 export const importEventsController = async (req, res) => {
     try {
@@ -11,5 +14,36 @@ export const importEventsController = async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({ error: "Erreur lors de l'importation des événements." });
+    }
+};
+
+export const getEventsByZone = async (req, res, next) => {
+    try {
+        // console.log('Zone param:', req.params.zone); // Log la zone reçue
+        const { zone } = req.params;
+        if (!zone) {
+            // console.log('Zone is missing');
+            return res.status(400).json({
+                success: false,
+                message: 'Zone is required',
+            });
+        }
+
+        const events = await getEventsByZoneService(zone);
+        // console.log('Events found:', events);
+        if (!events || events.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: 'No events found for this zone',
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: events,
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        next(error);
     }
 };
--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getEventsZone } from "../services/eventServices.js";
+// import { getEventsZone, getEventsByZone } from "../services/eventServices.js";
+import { getEventsByZone } from "../controllers/eventsController.js";
+
 
 const router = express.Router();
 
-router.get("/eventsByZone", getEventsZone);
+// router.get("/eventsByZone", getEventsZone);
+
+router.get('/events/zone/:zone', getEventsByZone);
 
 export default router;
